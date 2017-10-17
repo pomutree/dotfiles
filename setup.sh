41796d663fbc541cd2ee4fa9caaceb7f4b8e8d57
pomutree <daiga.suzuki@gmail.com>
2017-10-17 08:05:35 +0000

    setup.shを作成

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+user_name=`whoami`
+git_username="pomutree"
+git_user_email="daiga.suzuki@gmail.com"
+
+for dotfile in vimrc zshrc gitconfig 
+do
+  ln -vnfs $HOME/dotfiles/$dotfile $HOME/.$dotfile
+done
+
+# git設定
+git config --global user.name "${git_username}"
+git config --global user.email "${git_user_email}"
+git config --global color.ui auto
+
+# zsh設定 
+chsh -s /usr/bin/zsh ${user_name}
+
+# ホームディレクトリ以下の所有者を統一
+chown -R ${user_name}:${user_name} /home/${user_name}/
+
+# zshを利用
+zsh
+source $HOME/.zshrc
