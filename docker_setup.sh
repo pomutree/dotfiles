9730124a18cce94c24fa36cc5968565044e45018
pomutree <daiga.suzuki@gmail.com>
2020-04-17 14:34:35 +0000

    Added settings for Docker

---
+++ docker_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+user_name=`whoami`
+git_username="pomutree"
+git_user_email="daiga.suzuki@gmail.com"
+
+# owner
+if [ $user_name != 'root' ]; then
+    echo 'run as a root user.';
+    exit 1
+fi
+
+for dotfile in vimrc gitconfig 
+do
+  ln -vnfs $HOME/dotfiles/$dotfile $HOME/.$dotfile
+done
+
+ln -vnfs $HOME/dotfiles/zshrc.docker $HOME/.zshrc
